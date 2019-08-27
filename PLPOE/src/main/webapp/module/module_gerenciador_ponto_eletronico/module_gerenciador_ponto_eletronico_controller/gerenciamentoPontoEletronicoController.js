223b30bf1c326c29132a14e643ba09545668d849
Nihonium <nihonium@outlook.com.br>
2019-08-27 13:05:04 -0300

    [PLPOE-20190804030422]
    - Implementação dos protótipos do sistema
    
    - Ajuste no comprimento da Logo Marca
    
    - Implementação do controller 'gerenciadorPontoEletronicoController.js'

--- PLPOE/src/main/webapp/module/module_gerenciador_ponto_eletronico/module_gerenciador_ponto_eletronico_controller/gerenciamentoPontoEletronicoController.js
+++ PLPOE/src/main/webapp/module/module_gerenciador_ponto_eletronico/module_gerenciador_ponto_eletronico_controller/gerenciamentoPontoEletronicoController.js
@@ -1,3 +1,9 @@
-/**
- * 
- */
+var gerenciadorPontoEletronicoModule = angular.module("gerenciadorPontoEletronicoModule", []);
+
+gerenciadorPontoEletronicoModule.controller("gerenciadorPontoEletronicoController", function($scope) {
+	
+	$scope.moduleName = "Registro de Ponto Eletrônico";
+	
+	function incializador() { };
+	
+});
