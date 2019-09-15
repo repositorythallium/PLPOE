4b9e8309591cb114b6d73367a53ed9257320ab5b
Nihonium <nihonium@outlook.com.br>
2019-09-15 14:05:40 -0300

    [PLPOE-20190804030422]

--- PLPOE/src/main/webapp/module/module_gerenciador_ponto_eletronico/module_gerenciador_ponto_eletronico_controller/gerenciamentoPontoEletronicoController.js
+++ PLPOE/src/main/webapp/module/module_gerenciador_ponto_eletronico/module_gerenciador_ponto_eletronico_controller/gerenciamentoPontoEletronicoController.js
@@ -4,34 +4,66 @@ gerenciadorPontoEletronicoModelModule.controller("gerenciadorPontoEletronicoMode
 	
 	var NAME_API_PONTO_ELETRONICO = "http://localhost:8080/PLPOE-1.0.0.1-SNAPSHOT/rest/pontoEletronicoResource";
 	
-	$scope.gerenciadorPontoEletronicoModel = {};
+	$scope.gerenciadorPontoEletronicoModel = {
+		dataHoraPrimeiraEntrada: null,
+		dataHoraPrimeiraSaida: null,
+		dataHoraSegundaEntrada: null,
+		dataHoraSegundaSaida: null,
+		dataHoraAtual: null,
+	};
 	
 	$scope.gerenciadorPontoEletronicoList = [];
 	
 	function incializador() {
-		$scope.moduleName = "Registro de Ponto Eletrônico";
+//		$scope.moduleName = "Registro de Ponto Eletrônico";
+		$scope.findAll();
 	};
 	
 	$scope.save = function(gerenciadorPontoEletronicoModel) {
 		console.log(gerenciadorPontoEletronicoModel);
-		$http.post(NAME_API_PONTO_ELETRONICO);
+		gerenciadorPontoEletronicoModel.dataHoraAtual = $scope.recuperarDataAtualDDMMAAAA();
+		$scope.gerenciadorPontoEletronicoList.push(gerenciadorPontoEletronicoModel);
+//		$http.post(NAME_API_PONTO_ELETRONICO);
 		delete $scope.gerenciadorPontoEletronicoModel;
 	};
 	
 	$scope.update = function(gerenciadorPontoEletronicoModel) {
-		
+		$scope.gerenciadorPontoEletronicoModel = gerenciadorPontoEletronicoModel;
 	};
 	
-	$scope.delete = function(gerenciadorPontoEletronicoModel) {
-		
+	$scope.remove = function(gerenciadorPontoEletronicoModel) {
+		$scope.gerenciadorPontoEletronicoList.splice($scope.gerenciadorPontoEletronicoList.indexOf(gerenciadorPontoEletronicoModel), 1);
+		$scope.clear();
 	};
 	
 	$scope.findAll = function() {
-		
+		console.log($http.get(NAME_API_PONTO_ELETRONICO));
+		// $scope.gerenciadorPontoEletronicoList.push();
 	};
 	
 	$scope.findOne = function(gerenciadorPontoEletronicoModel) {
 		
+	};
+	
+	/* 
+		Retornar data atual no Formato DD/MM/AAAA
+	*/
+	$scope.recuperarDataAtualDDMMAAAA = function() {
+		return new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear();
+	};
+	
+	/* 
+		Retornar data atual no Formato DD/MM/AAAA HH:MM
+	*/
+	$scope.recuperarDataAtualDDMMAAAAHHMM = function() {
+		return new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear() + ' ' + new Date().getHours() + ':' + new Date().getMinutes();
+	};
+	
+	/*
+	 	Limpar dados do formulario
+	*/
+	$scope.clear = function() {
+		$scope.gerenciadorPontoEletronicoModel = null;
 	};
 	
 });
